8bcd1353bd8b666fe41d6c086f9f85e6dc0a1677
andre <andremendespr@gmail.com>
2026-09-06 22:58:59 -0300

    laala

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { Partners } from "@/components/Partners";
 import { Location } from "@/components/Location";
 import { Benefits } from "@/components/Benefits";
 import { BeforeAfter } from "@/components/BeforeAfter";
-import { ExclusiveOffer } from "@/components/ExclusiveOffer";
 import { Pricing } from "@/components/Pricing";
 import { GoogleReviews } from "@/components/GoogleReviews";
 import { Footer } from "@/components/Footer";
