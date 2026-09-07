5a0fd997d7df3ae55f4ba19005e0d80fd8de3b50
andre <andremendespr@gmail.com>
2026-09-06 22:35:30 -0300

    remove offer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       <Benefits />
       <Location />
       <BeforeAfter />
-      <ExclusiveOffer />
+     
       <Pricing />
       <GoogleReviews />
       <Footer />
